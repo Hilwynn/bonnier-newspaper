cde93c8ae2ef4eda75f4ffcb2fe2900d7d04d922
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-09-25 22:35:49 +0200

    Made filter function and buttons generic
    Changed the approach to the buttons to make them generic so we can use less code.
    
    Means we can also use one load button for all filters.

--- code/script.js
+++ code/script.js
@@ -1,31 +1,43 @@
+const fallbackImagesArray = [
+  "fallback-img-1.jpg",
+  "fallback-img-2.jpg",
+  "fallback-img-3.jpg",
+  "fallback-img-4.jpg",
+  "fallback-img-5.jpg",
+  "fallback-img-6.jpg"
+];
 
-function loadArticles(page, query) {
+// Choose random image file for fallback image
+const randomFallbackImage = () => {
+  let randomNumber = Math.floor(Math.random() * (fallbackImagesArray.length))
+  return fallbackImagesArray[randomNumber]
+}
+
+// Replace broken images with a random image from the array
+const imgError = (image) => {
+  //inside the file so we are creating an image everytime instead of setting one and using it all the time.
+  let randomImageSrc = randomFallbackImage()
+  console.log(randomImageSrc)
+  image.onerror = "";
+  image.src = `./images/${randomImageSrc}`;
+  return true;
+}
+
+
+
+function loadArticles(page, subquery) {
 
   // Setup of API fetching
   const language = "en"
-  // const query = "recipe+AND+cooking+AND+food+NOT+diet+NOT+restaurant+NOT+sports+NOT+chowhound+NOT+review"
+  let query = "recipe+AND+cooking+AND+food+NOT+diet+NOT+restaurant+NOT+sports+NOT+chowhound+NOT+review"
+
+  if (subquery !== undefined && subquery.length > 0) {
+    query += `+AND+${subquery}`
+  }
   const sortBy = "publishedAt"
   const apiKey = "8faa8f82d07f4d2791452c95b103bd5a"
 
   const urlArticles = `https://newsapi.org/v2/everything?language=${language}&q=${query}&sortBy=${sortBy}&apiKey=${apiKey}&page=${page}`
-
-  const fallbackImagesArray = [
-    "fallback-img-1.jpg",
-    "fallback-img-2.jpg",
-    "fallback-img-3.jpg",
-    "fallback-img-4.jpg",
-    "fallback-img-5.jpg",
-    "fallback-img-6.jpg"
-  ];
-
-  // Choose random image file for fallback image
-  const randomFallbackImage = () => {
-    let randomNumber = Math.floor(Math.random() * (fallbackImagesArray.length))
-    return fallbackImagesArray[randomNumber]
-  }
-
-  let randomImageSrc = randomFallbackImage()
-
 
   const recievedArticles = (newsdata) => {
 
@@ -85,41 +97,46 @@ function loadArticles(page, query) {
   }
 
   fetch(urlArticles).then(response => response.json()).then(recievedArticles)
-
-
-  // Replace broken images with a random image from the array
-  const imgError = (image) => {
-    console.log(randomImageSrc)
-    image.onerror = "";
-    image.src = `./images/${randomImageSrc}`;
-    return true;
-  }
 }
 
-loadArticles(1, "recipe+AND+cooking+AND+food+NOT+diet+NOT+restaurant+NOT+sports+NOT+chowhound+NOT+review");
 
+loadArticles(1, "");
+
+// current page
 let g_currentPage = 1
+let g_currentFilter = ""
+
+// load more function
+function loadMore() {
+  g_currentPage += 1;
+  loadArticles(g_currentPage, g_currentFilter);
+}
+
+//universal filter to feed into buttons
+function changeFilter(filter) {
+  document.querySelector(".grid").innerHTML = ""
+  g_currentPage = 1;
+  g_currentFilter = filter;
+  loadArticles(g_currentPage, g_currentFilter);
+}
+
 let button = document.getElementById("load-more")
 button.onclick = () => {
-    g_currentPage+=1
-    loadArticles(g_currentPage);
-  }
+  loadMore();
+}
 
+let buttonVegan = document.getElementById("vegan-button")
+buttonVegan.onclick = () => {
+  changeFilter("vegan");
+}
 
-  // Filter function for vegan
-  const filterFunction = () => {
-    document.querySelector(".grid").innerHTML = ""
-    loadArticles(1, "recipe+AND+cooking+AND+food+NOT+diet+NOT+restaurant+NOT+sports+NOT+chowhound+NOT+review+vegan")
+let buttonChicken = document.getElementById("chicken-button")
+buttonChicken.onclick = () => {
+  changeFilter("chicken");
+}
 
-  }
-
-  let buttonVegan = document.getElementById("vegan-button")
-  buttonVegan.onclick = () => {
-      filterFunction();
-    }
-
-  // Filter button for "All"
-  let buttonAll = document.getElementById("all-articles-button")
-    buttonAll.onclick = () => {
-      loadArticles(1, "recipe+AND+cooking+AND+food+NOT+diet+NOT+restaurant+NOT+sports+NOT+chowhound+NOT+review");
-    }
+// Filter button for "All"
+let buttonAll = document.getElementById("all-articles-button")
+buttonAll.onclick = () => {
+  changeFilter("");
+}
